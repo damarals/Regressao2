5d9ff8cb63b7239bab2a07f0cf5d8dfc9998cf93
Daniel Amaral <silva.daniel86@Gmail.com>
2019-04-09 16:17:06 -0300

    Add files via upload

---
+++ 09042019.R
@@ -0,0 +1,33 @@
+Tipo = factor(c(rep('BAP',90),rep('TDZ',90)))
+Nivel = factor(rep(c(rep(5,30), rep(1,30),rep(0.1,30)),2))
+Auxina = factor(rep(c(rep('NAA',10),rep('IBA',10),rep('2-4D',10)),6))
+Bloco = factor(rep(1:10, 18))
+y = c(1,1,0,0,1,0,1,0,1,1,0,1,1,1,1,1,0,1,1,1,1,1,1,1,1,1,1,0,0,1,0,0,0,0,0,0,0,0,0,0,1,1,1,0,0,1,1,0,1,1,1,0,1,1,0,1,1,1,1,1,
+0,0,1,1,1,0,1,0,0,0,0,0,0,1,1,1,1,0,1,0,0,0,1,1,1,1,1,0,1,1,1,1,1,1,1,0,1,1,1,1,1,1,1,1,1,1,1,1,1,1,
+1,0,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,0,1,1,1,1,1,1,1,0,1,1,
+1,1,1,1,1,1,0,1,1,1,0,0,1,0,1,1,1,1,1,1)
+
+m = 1
+resp = cbind(y, m-y)
+model = glm(resp ~ Tipo * Nivel * Auxina + Bloco, family = binomial(link='logit'))
+summary(model)
+
+model = glm(resp ~ Bloco, family = binomial(link='logit'))
+summary(model)
+anova(model, test = 'Chisq')
+
+model = glm(resp ~ Bloco + Nivel, family = binomial(link='logit'))
+summary(model)
+anova(model, test = 'Chisq')
+
+model = glm(resp ~ Bloco + Tipo, family = binomial(link='logit'))
+summary(model)
+anova(model, test = 'Chisq')
+
+model = glm(resp ~ Bloco + Tipo + Nivel, family = binomial(link='logit'))
+summary(model)
+anova(model, test = 'Chisq')
+
+model = glm(resp ~ Bloco + Tipo*Nivel*Auxina, family = binomial(link='logit'))
+summary(model)
+anova(model, test = 'Chisq')
